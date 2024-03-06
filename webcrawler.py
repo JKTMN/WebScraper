c41a4be631e761c686fd51394f8e3fc3a600182b
Jamie <111710857+JKTMN@users.noreply.github.com>
2024-03-06 01:08:04 +0000

    Update to the webcrawler
    might have updated some code
    added comments to code
    known issues and ideas stored at bottom

--- webcrawler.py
+++ webcrawler.py
@@ -19,94 +19,58 @@ class webCrawler():
         self.emailAddressList = emailAddressList
         self.pagesOpened = 0
 
-    def firstWebpage(self):
+    def firstWebpage(self): #sets url to user input
         self.URL = input("Enter the target webpage: ")
 
     def getNextPage(self):
         self.driver.get(self.URL)
-        pages = self.driver.find_elements(By.TAG_NAME, "a")
-        for page in pages:
-            href = page.get_attribute("href")
-            if href and not href.startswith("#") and href not in self.pagesList:
-                self.pagesList.append(href) 
-                self.pagesOpened += 1
-            if self.pagesOpened >= 10:  # Check if 10 pages are visited
+        anchors = self.driver.find_elements(By.TAG_NAME, "a") #finds all anchors within the loaded webpage
+        for anchor in anchors: #filters through the webpages
+            link = anchor.get_attribute("href") #checks to see if anchor has 
+            if link and not link.startswith("#") and link not in self.pagesList: #check to see if link is true
+                #then checks to see if the link starts with a "#" and then checks to see if the link is already stored
+                self.pagesList.append(link) #adds link to list
+                self.pagesOpened += 1 #increments counter
+            if self.pagesOpened >= 10:  #check to see if 10 pages are visited
                 break
         return self.pagesList
     
     def scrapeEmailAddresses(self):
         self.driver.get(self.URL)
-        html = self.driver.page_source  # Fixed: Changed 'driver' to 'self.driver'
-        soup = BeautifulSoup(html, "html.parser")
-        emailAddresses = soup.find_all(string=lambda text: "@" in text)
-        for emailAddress in emailAddresses:
-            if emailAddress and emailAddress not in self.emailAddressList:
-                self.emailAddressList.append(emailAddress.text)
+        html = self.driver.page_source  #sets a variable to be the webpage source
+        soup = BeautifulSoup(html, "html.parser") #uses bs4 to parse the html
+        emailAddresses = soup.find_all(string=lambda text: "@" in text) #searches the html to find strings containing "@"
+        for emailAddress in emailAddresses: #filters through found email address
+            if emailAddress and emailAddress != "" and emailAddress not in self.emailAddressList:
+                #checks to see if emailadress is true, it is not empty and not already stores
+                self.emailAddressList.append(emailAddress.text) #stores email adress
 
     def openPageList(self):
-        for page in self.pagesList:
-            self.URL = page
-            self.scrapeEmailAddresses()
-            self.getNextPage()
+        for page in self.pagesList: #filters through the links 
+            self.URL = page #sets the new url to the next link stored in the list
+            self.scrapeEmailAddresses() #calls the scrapeEmailAddress function
+            self.getNextPage() #calls the getNextPage function
 
+#inititating selenium objects
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+#initiating empty lists
 pagesList = []
 emailAddressList = []
 
+#initiating object and calling functions
 Crawler = webCrawler(service, driver, "", pagesList, emailAddressList)
 Crawler.firstWebpage()
 Crawler.getNextPage()
 Crawler.openPageList()
 print("Email addresses are:")
-print(str(Crawler.emailAddressList))
+print(Crawler.emailAddressList)
+print(Crawler.pagesList)
 
-### above is old code which stopped working
+#Issues
+##duplicate links stored
+##empty email addresses stored
 
-### trying to fix ciode below
-
-# class webCrawler():
-#     def __init__(self, service, driver, URL, pagesList, emailAddressList):
-#         self.service = service
-#         self.driver = driver
-#         self.URL = URL
-#         self.pagesList = pagesList
-#         self.emailAddressList = emailAddressList
-
-#     def firstWebpage(self): #sets the url to a user input
-#         self.URL = input("Enter the target webpage: ")
-
-#     def getNextPage(self):
-#         self.driver.get(self.URL) #searches for anchors within the loaded webpage
-#         pages = self.driver.find_elements(By.TAG_NAME, "a")
-
-#         for page in pages: #filters through anchors
-#             href = page.get_attribute("href") #finds anchors with links
-#             if href and href.startswith("#") == False and href not in self.pagesList: #checks to see if link is true and 
-#                 #checks to see if the link starts with a "#" finally checks to see if duplicate
-#                 self.pagesList.append(href) #adds link to the list of webpages
-#             else:
-#                 break 
-#         return self.pagesList
-    
-#     def scrapeEmailAddresses(self):
-#         self.driver.get(self.URL)
-#         html = driver.page_source
-#         driver.close()
-#         soup = BeautifulSoup(html, "html.parser")
-#         emailAddresses = soup.find_all(string=lambda text: "@" in text)
-#         for emailAddress in emailAddresses:
-#             if emailAddress and emailAddress not in self.emailAddressList:
-#                 self.emailAddressList.append(emailAddress.text)
-#         return self.emailAddressList
-
-
-# service = Service(executable_path="chromedriver.exe")
-# driver = webdriver.Chrome(service=service)
-# pagesList = []
-# emailAddressList = []
-
-# Crawler = webCrawler(service, driver, "", pagesList, emailAddressList)
-# Crawler.firstWebpage()
-# print(Crawler.scrapeEmailAddresses())
+#ideas
+##create variation of this where it stores the links to the courses instead of any link to webpage
