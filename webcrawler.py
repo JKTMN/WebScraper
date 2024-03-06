e70aef03bc0da5edb9e6433aad1d54ccc0e5744c
Jamie <111710857+JKTMN@users.noreply.github.com>
2024-03-06 14:41:47 +0000

    working webscraper kinda
    This webscraper is hardcoded to search through the staff section of solent university and scrape emails. further testing is needed to see if it works for others as it works based off url, however it is hard coded at 35 pages which is the page limit for the solent university staff pages
    
    Changes that were made:
    changed the logic of how the scraper works
    changed the next page function to use pageation...kinda
    
    to test, use this link "https://www.solent.ac.uk/staff"

--- webcrawler.py
+++ webcrawler.py
@@ -6,71 +6,64 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import re
+from validate_email import validate_email
 
-
-#URL = "https://www.solent.ac.uk/courses/undergraduate/education-ba/2024/ba-hons-education-3-years-2024"
 
 class webCrawler():
-    def __init__(self, service, driver, URL, pagesList, emailAddressList):
+    def __init__(self, service, driver, URL, emailAddressList):
         self.service = service
         self.driver = driver
         self.URL = URL
-        self.pagesList = pagesList
         self.emailAddressList = emailAddressList
         self.pagesOpened = 0
 
     def firstWebpage(self): #sets url to user input
         self.URL = input("Enter the target webpage: ")
 
-    def getNextPage(self):
-        self.driver.get(self.URL)
-        anchors = self.driver.find_elements(By.TAG_NAME, "a") #finds all anchors within the loaded webpage
-        for anchor in anchors: #filters through the webpages
-            link = anchor.get_attribute("href") #checks to see if anchor has 
-            if link and not link.startswith("#") and link not in self.pagesList: #check to see if link is true
-                #then checks to see if the link starts with a "#" and then checks to see if the link is already stored
-                self.pagesList.append(link) #adds link to list
-                self.pagesOpened += 1 #increments counter
-            if self.pagesOpened >= 10:  #check to see if 10 pages are visited
+    def click_next_page(self):
+        current_page = 1
+        while True:
+            next_page_url = f"{self.URL}?page={current_page}"
+            self.driver.get(next_page_url)
+            self.wait_for_page_load()  #Wait for the page to load
+            self.scrapeEmailAddresses()  #Scrape email addresses from the current page
+            current_page += 1
+            self.pagesOpened += 1
+            if self.pagesOpened >= 35:  #Stop after opening 35 pages
                 break
-        return self.pagesList
-    
+
     def scrapeEmailAddresses(self):
-        self.driver.get(self.URL)
         html = self.driver.page_source  #sets a variable to be the webpage source
-        soup = BeautifulSoup(html, "html.parser") #uses bs4 to parse the html
-        emailAddresses = soup.find_all(string=lambda text: "@" in text) #searches the html to find strings containing "@"
-        for emailAddress in emailAddresses: #filters through found email address
-            if emailAddress and emailAddress != "" and emailAddress not in self.emailAddressList:
-                #checks to see if emailadress is true, it is not empty and not already stores
-                self.emailAddressList.append(emailAddress.text) #stores email adress
+        soup = BeautifulSoup(html, "html.parser")  #uses bs4 to parse the html
+        emailAddresses = soup.find_all(string=lambda text: "@" in text)  #searches the html to find strings containing "@"
+        for emailAddress in emailAddresses:  #filters through found email addresses
+            if emailAddress and validate_email(emailAddress) and emailAddress not in self.emailAddressList:
+                #checks to see if email address is true, it is a valid email address and not already stored
+                self.emailAddressList.append(emailAddress.text)  #stores email address
 
     def openPageList(self):
-        for page in self.pagesList: #filters through the links 
-            self.URL = page #sets the new url to the next link stored in the list
-            self.scrapeEmailAddresses() #calls the scrapeEmailAddress function
-            self.getNextPage() #calls the getNextPage function
+        self.click_next_page()  #calls the click_next_page function
 
-#inititating selenium objects
+    def wait_for_page_load(self):
+        WebDriverWait(self.driver, 10).until(
+            lambda driver: driver.execute_script('return document.readyState') == 'complete'
+        )
+
+
+
+# Initialize the service and driver
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-#initiating empty lists
-pagesList = []
+# Initializing empty lists
 emailAddressList = []
 
-#initiating object and calling functions
-Crawler = webCrawler(service, driver, "", pagesList, emailAddressList)
+# Initializing object and calling functions
+Crawler = webCrawler(service, driver, "", emailAddressList)
+
 Crawler.firstWebpage()
-Crawler.getNextPage()
 Crawler.openPageList()
+
 print("Email addresses are:")
 print(Crawler.emailAddressList)
-print(Crawler.pagesList)
-
-#Issues
-##duplicate links stored
-##empty email addresses stored
-
-#ideas
-##create variation of this where it stores the links to the courses instead of any link to webpage
